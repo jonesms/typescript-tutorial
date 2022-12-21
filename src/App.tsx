cd335bfa715d6c53853a6cef3081bdb540d14775
jonesms <marks.jones26@gmail.com>
2022-12-21 15:05:55 +0000

    added App.css and Added placeholder for input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,14 @@
+import { constants } from 'buffer';
 import React from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
-let name: string;
-let age: number;
-let age2: number | string; // a Union age can be either a string or number
-let isStudent: boolean;
-let hobbies:string[];
-let role:[number,string]; // tuple
 
-name = "Mark";
-role=[5,"bill"];
-
-age2= "tweny one";
-age2 = 21;
-
-//define an object
-type Person = {
-  name:string;
-  age?:number; //? means optional
-};
-
-let person: Person = {
-  name:"mark",
- // age: 62
-};
-
-let printName2: (name:string) => void; //function dec which returns void
-
-let printName3: (name:string) => never; //function dec which returns nothing
-
-let name2:unknown; // name2 can contain any type. string number object etc
-
-function printName (name:string) {
-  console.log(name);
-}
-
-printName("Stephen");
-
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
+      <span className="heading">Taskify</span>
+      <InputField/>
     </div>
   );
 }
