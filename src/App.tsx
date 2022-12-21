fe35a29999f1501104402d34b0e7bb92b8f0d336
jonesms <marks.jones26@gmail.com>
2022-12-21 15:58:35 +0000

    added state to components and model interface

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import { constants } from 'buffer';
 import React from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { useState } from 'react';
 
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+
+  console.log(todo);
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField/>
+      <InputField todo={todo} setTodo={setTodo}/>
     </div>
   );
 }
