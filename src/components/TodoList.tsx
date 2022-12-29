c1927c18cb0db4e50a861dd30b93b42bf016bdb6
jonesms <marks.jones26@gmail.com>
2022-12-29 16:14:02 +0000

    added completed section/div

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -10,18 +10,24 @@ interface Props{
 
 const TodoList: React.FC<Props> = ({todos, setTodos}) => {
   return (
-    <div className="todos">
-      {
-        todos.map((todo) => (
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading">Active Tasks</span>
+        {todos.map((todo) => (
           <SingleTodo 
-            todo={todo}
-            key={todo.id}
-            todos={todos}
-            setTodos={setTodos}
-          />
-        ))
-      }
+          todo={todo}
+          todos={todos}
+          key={todo.id}
+          setTodos={setTodos}
+        />
+
+        ))}
+      </div>
+      <div className="todos remove">
+        <span className="todos__heading">Active Tasks</span>
+      </div>
     </div>
+
   )
 }
 
