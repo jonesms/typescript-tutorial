7c67e6972efce4257d589b3b8a2535741112f547
jonesms <marks.jones26@gmail.com>
2023-01-03 16:35:08 +0000

    Added react-beautiful-dnd and dependencies, started creating draggble sections in UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import InputField from './components/InputField';
 import { useState } from 'react';
 import { Todo } from './model';
 import TodoList from './components/TodoList';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 
 const App: React.FC = () => {
@@ -12,6 +13,8 @@ const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
 
   const [todos, setTodos] = useState<Todo[]>([]);
+
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
@@ -25,12 +28,22 @@ const App: React.FC = () => {
     console.log(todos);
   },[todos]);
 
+  const handleOnDragEnd = () => {
+    console.log("dragEnd");
+  }
+
   return (
+    <DragDropContext onDragEnd={handleOnDragEnd}>
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-      <TodoList todos={todos} setTodos={setTodos}/>
+      <TodoList 
+        todos={todos} 
+        setTodos={setTodos}
+        completedTodos={completedTodos}
+        setCompletedTodos={setCompletedTodos}/>
     </div>
+    </DragDropContext>
   );
 }
 
